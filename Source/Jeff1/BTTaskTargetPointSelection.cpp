d14d32e04133e2efbbd48ed3a146fb2528bc8ad4
Thomas <verhile.tomas@orange.fr>
2021-10-10 20:59:54 +0200

    removed useless line

--- Source/Jeff1/BTTaskTargetPointSelection.cpp
+++ Source/Jeff1/BTTaskTargetPointSelection.cpp
@@ -20,17 +20,14 @@ EBTNodeResult::Type UBTTaskTargetPointSelection::ExecuteTask(UBehaviorTreeCompon
 		ABotTargetPoint* CurrentPoint = Cast<ABotTargetPoint>(BlackboardComp->GetValueAsObject("LocationToGo"));
  
 		TArray<AActor*> AvailableTargetPoints = AICon->GetAvailableTargetPoints();
-       
-		//This variable will contain a random index in order to determine the next possible point
-		int32 RandomIndex;
- 
+
 		//Here, we store the possible next target point
 		ABotTargetPoint* NextTargetPoint = nullptr;
  
 		//Find a next point which is different from the current one
 		do
 		{
-			RandomIndex = FMath::RandRange(0, AvailableTargetPoints.Num()-1);
+			const int32 RandomIndex = FMath::RandRange(0, AvailableTargetPoints.Num() - 1);
 			//Remember that the Array provided by the Controller function contains AActor* objects so we need to cast.
 			NextTargetPoint = Cast<ABotTargetPoint>(AvailableTargetPoints[RandomIndex]);
 		} while (CurrentPoint == NextTargetPoint);
